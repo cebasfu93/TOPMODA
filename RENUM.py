b0c4269b93ab1b785103db4c6cf841f4adeba377
Sebastian Franco Ulloa <cebasfu93@gmail.com>
2018-01-31 10:10:09 +0100

    Now accepts gro with multiple structures but only the membrane must be present

--- RENUM.py
+++ RENUM.py
@@ -12,15 +12,17 @@ outname_opt=options.OutputFile
 
 def renum(gro_file_func):
     gro_file=np.genfromtxt(gro_file_func, delimiter="\n", dtype="str")
-    temp_file=open("temp.gro", "a")
+    out_file=open(outname_opt, "a")
 
     N_gro=len(gro_file)
-    atom=1
-    residue=1
-    res_act="PA"
+    N_one=int(gro_file[1])+3
+
     for i in range(N_gro):
-        if i==0 or i ==1 or i==(N_gro-1):
-            temp_file.write(gro_file[i]+"\n")
+        if i%N_one==0 or i%N_one ==1 or i%N_one==(N_one-1):
+            out_file.write(gro_file[i]+"\n")
+            res_act="PA"
+            atom=1
+            residue=1
         else:
             line_act=gro_file[i]
             res_prev=res_act
@@ -28,31 +30,17 @@ def renum(gro_file_func):
                 res_act="PA"
                 if res_act!=res_prev:
                     residue+=1
-                temp_file.write(str(residue).rjust(5)+"PA".ljust(5)+line_act[-58:]+"\n")
+                out_file.write(str(residue).rjust(5)+"PA".ljust(5)+line_act[-58:]+"\n")
             elif "PC" in line_act:
                 res_act="PC"
                 if res_act!=res_prev:
                     residue+=1
-                temp_file.write(str(residue).rjust(5)+"PC".ljust(5)+line_act[-58:]+"\n")
-            elif "SOL" in line_act:
-                break
+                out_file.write(str(residue).rjust(5)+"PC".ljust(5)+line_act[-58:]+"\n")
             elif "OL" in line_act:
                 res_act="OL"
                 if res_act!=res_prev:
                     residue+=1
-                temp_file.write(str(residue).rjust(5)+"OL".ljust(5)+line_act[-58:]+"\n")
-    temp_file.write(gro_file[-1])
-    temp_file.close()
-
-    out_file=open(outname_opt, "a")
-    temp_gro=np.genfromtxt("temp.gro", delimiter="\n", dtype="str")
-    N_out=len(temp_gro)
-    for i in range(N_out):
-        if i==1:
-            out_file.write(str(N_out-3)+"\n")
-        elif i==0 or i==N_out-1:
-            out_file.write(temp_gro[i]+"\n")
-        else:
-            out_file.write(temp_gro[i].rjust(68)+"\n")
+                out_file.write(str(residue).rjust(5)+"OL".ljust(5)+line_act[-58:]+"\n")
     out_file.close()
+
 renum(inname_opt)
